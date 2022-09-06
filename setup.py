3e540ca202cc85175f40e4f023366d770053da94
Feyzi Kesim <36535648+feyzikesim@users.noreply.github.com>
2022-09-06 13:12:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup (name = 'pyds_analytics',
        version = '1.0',
-       description = """Install precompiled DeepStream Python bindings for tracker metadata extension""",
+       description = """Install precompiled DeepStream Python bindings for analytics metadata extension""",
        packages=[''],
        package_data={'': ['pyds_analytics.so']},
        )
